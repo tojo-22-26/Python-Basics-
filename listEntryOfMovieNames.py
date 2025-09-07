0040b693da03cd36139475126e2ea158a5502556
tojo-22-26 <atandraghosh08@gmail.com>
2025-09-07 13:36:50 +0530

    07-09-25

--- listEntryOfMovieNames.py
+++ listEntryOfMovieNames.py
@@ -2,12 +2,12 @@
 while True:
     i=0
     name=input("Enter your Name: ")
-    print("Enter the 3 favourite movie names: \n")
+    print("Enter the 3 favorite movie names: \n")
     list1=[]
     for i in range(0,3):
         mov=input("Enter here: ")
         list1.append(mov)
-    print("The 3 favorite movies of",name," is:",list1)
+    print("The 3 favorite movies of",name,"is:",list1)
     retry=input("Want to continue? (Y/N):").lower()
     if(retry!='y'):
         break
