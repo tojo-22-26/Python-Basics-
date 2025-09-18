c4c9ed54b7a9899a7bb019e27b16f5f9f89c7108
tojo-22-26 <atandraghosh08@gmail.com>
2025-09-18 22:52:29 +0530

    Broadcasting

---
+++ broadcasting.py
@@ -0,0 +1,10 @@
+import numpy as np
+var1=np.array([[1],[2],[3]])
+print(var1.shape)
+print()
+var2=np.array([1,2,3],)
+print(var2.shape)
+print()
+print(var1+var2)
+
+#the column must be either same or either of the columns should be one
